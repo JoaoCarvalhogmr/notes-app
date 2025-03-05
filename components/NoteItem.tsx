420421ebb45a425198b3e36ed0ac27d5a58eaf5b
João Carvalho <joaocarvalhoum@gmail.com>
2025-03-05 13:49:41 +0000

    initial commit

---
+++ components/NoteItem.tsx
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet } from "react-native"
+
+type NoteItemProps = {
+  text: string
+}
+
+const NoteItem = (item: NoteItemProps) => {
+  return (
+    <View style={styles.noteItem}>                    
+        <Text style={styles.noteText}>{item.text}</Text>
+    </View>  )
+}
+
+const styles = StyleSheet.create({
+  noteItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#f5f5f5",
+    padding: 15,
+    borderRadius: 5,
+    marginVertical: 5,
+},
+  noteText: {
+    fontSize: 18
+  },
+})
+
+export default NoteItem
